ff05b44002ef7e961a9a5c30e37e0c41b883dfd1
RicardoGT0 <rgonzaleztello@live.com.mx>
2023-03-31 17:43:47 -0600

    cambio en mensaje

--- src/controllers/activities/createActivities.js
+++ src/controllers/activities/createActivities.js
@@ -1,11 +1,11 @@
 const { Activity } = require('../../db.js');
 
 module.exports=async (data)=>{
-    //data={method,state,from,to,message,subject,attached,sale_id,clientId,salesmanId,}
+    //data={method,state,from,to,message,subject,attached,clientId,salesmanId,}
     if (data.salesmanId != null && data.clientId != null) {
         const newActivity = await Activity.create(data)
         return newActivity;
     } else {
-        throw new Error('salesmanId or client_id is undefined')
+        throw new Error('salesmanId or clientId is undefined')
     }
 }
