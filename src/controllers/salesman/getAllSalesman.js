62d8f93a09e280cdf9dd6b389afe162b83782bd9
RicardoGT0 <rgonzaleztello@live.com.mx>
2023-04-04 12:10:26 -0600

    Controlador y handler GET Vendedor por BossID

--- src/controllers/salesman/getAllSalesman.js
+++ src/controllers/salesman/getAllSalesman.js
@@ -1,7 +1,22 @@
 const { Salesman } = require("../../db.js");
 
-const getAllSalesman = async () => {
-  const allSalesman = await Salesman.findAll();
+const getAllSalesman = async (data) => {
+  const { id, name, address, email, phone, enable, bossId } = data
+
+  if (id) {
+    const salesman = await Salesman.findByPk(id);
+    return salesman;
+  }
+
+  if (name || address || email || phone || enable) {
+    delete data.bossId
+    const variable = name || address || email || phone || enable;
+    const [propiedad] = Object.keys(data);
+    const allSalesman = await Salesman.findAll({ where: { bossId, [propiedad]: variable } });
+    return allSalesman;
+  }
+  
+  const allSalesman = await Salesman.findAll({ where: { bossId } });
   return allSalesman;
 };
 
