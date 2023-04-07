0ea3a24e9f4ba4c096ebd954af886d95baa5cb9b
RicardoGT0 <rgonzaleztello@live.com.mx>
2023-04-07 12:01:18 -0600

    correccion mensaje de error

--- src/controllers/Bosses/getBossById.js
+++ src/controllers/Bosses/getBossById.js
@@ -2,7 +2,7 @@ const { Boss } = require('../../db.js');
 
 module.exports = async (id) => {
     if (!id)
-        throw new Error('id activity\'s, clientId or salesmanId required')
+        throw new Error('(id) Boss required')
 
     if (id) {
         const boss = await Boss.findByPk(id);
