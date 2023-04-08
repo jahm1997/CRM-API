2622a164246666900f54c1aeeee5c369224411f5
RicardoGT0 <rgonzaleztello@live.com.mx>
2023-04-08 13:15:33 -0600

    correccion login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 const { Router } = require("express");
 // Importar todos los routers;
-const signUpRouter = require("./signupRouter.js");
 const loginRouter = require("./loginRouter.js");
 const bossRouter = require("./bossRouter.js");
 const clientsRouter = require("./clientsRouter.js");
@@ -28,7 +27,6 @@ router.use("/", sale_productsRouter);
 router.use("/", taskRouter);
 router.use("/", categoryRouter);
 router.use("/", dashboard_salesmanRouter);
-router.use("/", signUpRouter);
 router.use("/", loginRouter);
 router.use("/", dashboard_bossRouter);
 router.use('/', paymentRouter);
