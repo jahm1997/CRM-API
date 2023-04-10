0fa85eb1c45a9e8b6375d47df7d70982a0ce709f
HanJian16 <jhohanjianpierre@gmail.com>
2023-04-10 13:00:17 -0500

    cambios de CORS

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const routes = require("./routes/index.js");
 const multer = require("multer");
 const path = require("path");
+const cors = require('cors');
 
 require("./db.js");
 
@@ -12,20 +13,37 @@ const server = express();
 
 server.name = "API";
 
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept'],
+  credentials: true,
+};
+
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// server.use((req, res, next) => {
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept'],
+//     credentials: true,
+//     preflightContinue: true
+//   })
+  
+//   // res.header("Access-Control-Allow-Origin", "http://localhost:3000/authentication"); // update to match the domain you will make the request from
+//   // res.header("Access-Control-Allow-Credentials", "true");
+//   // res.header(
+//   //   "Access-Control-Allow-Headers",
+//   //   "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   // );
+//   // res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
+
+server.use(cors(corsOptions));
 
 server.set("src", path.join(__dirname, "src"));
 server.set("src engine", "ejs");
