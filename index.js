117c521c8ca893ff439e57580a8e034d9b1a856c
jahm1997 <jahm1997@gmail.com>
2023-04-16 17:37:54 -0500

    modificaciones joseph

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const notifyTask = require("./src/controllers/tasks/notifyTask.js");
 cron.schedule("0 9 * * *", () => {
   // SI COLOCAMOS * * * * * * se enviarian cada un segundo
   // TODOS LOS DIAS A LAS 9 de la mañana
+
   notifyTask();
 });
 
