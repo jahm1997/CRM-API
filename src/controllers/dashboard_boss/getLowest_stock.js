2ccbc4e28d9fd6e8e487ba2df95b358c150b20dc
jahm1997 <jahm1997@gmail.com>
2023-04-17 11:35:06 -0500

    correcion dasboard bos

--- src/controllers/dashboard_boss/getLowest_stock.js
+++ src/controllers/dashboard_boss/getLowest_stock.js
@@ -2,24 +2,26 @@ const { Boss, Product } = require("../../db.js");
 const { where, Op } = require("sequelize");
 
 module.exports = async (id) => {
-    const allProducts = await Boss.findByPk(id,
-        {
-            include: [
-                {
-                    model: Product
-                }
-            ]
-        }
-    )
+  const allProducts = await Boss.findByPk(id, {
+    include: [
+      {
+        model: Product,
+      },
+    ],
+  });
 
-    const products = allProducts.dataValues.products
+  if (allProducts.length) {
+    const products = allProducts.dataValues.products;
     let lowest_stock = products.map((p) => {
-        const { name, quantity, discount } = p.dataValues
-        return { name, quantity, discount }
-    })
+      const { name, quantity, discount } = p.dataValues;
+      return { name, quantity, discount };
+    });
 
-    lowest_stock.sort((x, y) => x.quantity - y.quantity)
-    lowest_stock = lowest_stock.slice(0, 10)
+    lowest_stock.sort((x, y) => x.quantity - y.quantity);
+    lowest_stock = lowest_stock.slice(0, 10);
 
-    return lowest_stock
-}
+    return lowest_stock;
+  } else {
+    return [];
+  }
+};
